bedce1d0eda87ec9d6b44cd5adb2b8dfdc0acb9f
JJ <sahib.julka@yahoo.com>
2023-05-24 03:05:53 +0200

    refactor dataprep

--- dataprep.py
+++ dataprep.py
@@ -5,13 +5,11 @@
 __author__      = "Sahib Julka <sahib.julka@uni-passau.de>"
 __copyright__   = "GPL"
 
+
 import os
-from PIL import Image
-from itertools import product
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
-from tifffile import imsave
+
 
 # replace the paths below to match local data dir
 DIR_IN = os.path.join(os.getcwd(), '../chorus_wave/npy_2/chorus')
@@ -21,6 +19,41 @@ if not os.path.exists(DIR_OUT):
 if not os.path.exists(DIR_IN):
     DIR_IN = input("Please enter input data path:")
 
+
+def convert_to_img(filename, dir_in, dir_out):
+    """
+    Converts a numpy array to an image and saves it as a PNG file.
+
+    Args:
+        filename (str): Name of the input numpy array file.
+        dir_in (str): Directory path of the input numpy array file.
+        dir_out (str): Directory path to save the output PNG file.
+    """
+    name, ext = os.path.splitext(filename)
+    # Load the input numpy array
+    img_arr = np.load(os.path.join(dir_in, filename))
+    # Apply logarithm transformation
+    img_log = np.log10(img_arr)
+    # Get the dimensions of the image
+    height, width = img_log.shape
+    dpi = 125  # Dots per inch for the output image
+
+    # Set the output file path
+    out = os.path.join(dir_out, f'{name}.png')
+    # Create a figure with the appropriate dimensions
+    fig = plt.figure(figsize=(width / 100, height / 100))
+
+    # Display the image
+    plt.imshow(img_log)
+    plt.axis('off')  # Turn off the axes
+    plt.savefig(out, dpi=dpi, bbox_inches='tight', pad_inches=0.0)
+
+    #close the image
+    plt.close(fig)
+    # Uncomment the line below if you want to save the cropped image array as a numpy file
+    # np.save(out, np.array(img_))
+    
+ 
 # def tile(filename, dir_in, dir_out):
 #     name, ext= os.path.splitext(filename)
 
@@ -42,54 +75,6 @@ if not os.path.exists(DIR_IN):
 #         plt.savefig(out, bbox_inches = 'tight')
 #         #np.save(out, np.array(img_))  # Save the cropped image array as numpy file
 
-
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-def convert_to_img(filename, dir_in, dir_out):
-    """
-    Converts a numpy array to an image and saves it as a PNG file.
-
-    Args:
-        filename (str): Name of the input numpy array file.
-        dir_in (str): Directory path of the input numpy array file.
-        dir_out (str): Directory path to save the output PNG file.
-    """
-    name, ext = os.path.splitext(filename)
-
-    # Load the input numpy array
-    img_arr = np.load(os.path.join(dir_in, filename))
-
-    # Apply logarithm transformation
-    img_log = np.log10(img_arr)
-
-    # Get the dimensions of the image
-    height, width = img_log.shape
-
-    dpi = 125  # Dots per inch for the output image
-
-    # Set the output file path
-    out = os.path.join(dir_out, f'{name}.png')
-
-    # Create a figure with the appropriate dimensions
-    fig = plt.figure(figsize=(width / 100, height / 100))
-
-    # Display the image
-    plt.imshow(img_log)
-
-    plt.axis('off')  # Turn off the axes
-
-
-
-
-    plt.savefig(out, dpi=dpi, bbox_inches='tight', pad_inches=0.0)
-
-    #close the image
-    plt.close(fig)
-    # Uncomment the line below if you want to save the cropped image array as a numpy file
-    # np.save(out, np.array(img_))
 
 if __name__ == "__main__":
     for image in os.listdir(DIR_IN):
